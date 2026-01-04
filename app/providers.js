a82a56547cffa53f44ca43de09c4f5e63e8ab892
Denis Kalenga <deniskalenga94@gmail.com>
2026-01-04 23:52:32 +0100

    Fix refresh issue

--- app/providers.js
+++ app/providers.js
@@ -1,16 +1,18 @@
 'use client';
 
 import { Amplify } from 'aws-amplify';
-import { useEffect } from 'react';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ToastProvider } from '@/contexts/ToastContext';
 import awsConfig from '@/lib/amplify-config';
 
-export default function Providers({ children }) {
-  useEffect(() => {
-    Amplify.configure(awsConfig);
-  }, []);
+let amplifyConfigured = false;
 
+if (!amplifyConfigured) {
+  Amplify.configure(awsConfig);
+  amplifyConfigured = true;
+}
+
+export default function Providers({ children }) {
   return (
     <AuthProvider>
       <ToastProvider>{children}</ToastProvider>
